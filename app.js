497325e27ae426d07bd302528a108f245507798a
PaulineB29 <104891500+PaulineB29@users.noreply.github.com>
2025-11-28 15:40:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -447,7 +447,6 @@ const ANALYSIS_DESCRIPTIONS = {
           ],
           expanded: false
         }
-        expanded: false
       },
       {
         title: "Comment utiliser ces informations",
